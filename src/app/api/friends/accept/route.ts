8960b00877922f97cd731766eaafa7e0feab5e23
poojan <pnd992@gmail.com>
2025-02-28 19:20:40 +0530

    dashboard completed

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/app/helper/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { z, ZodError } from "zod";
 
@@ -28,6 +30,12 @@ export async function POST(req: Request) {
         const hasFriendRequest = JSON.parse(hasFriendRequestData) as 0 | 1;
         if (!hasFriendRequest)
             return new Response("No friend request", { status: 400 });
+
+        pusherServer.trigger(
+            toPusherKey(`user:${idToAdd}:friends`),
+            "new_friend",
+            {}
+        );
 
         await db.sadd(`user:${session.user.id}:friends`, idToAdd);
         await db.sadd(`user:${idToAdd}:friends`, session.user.id);
--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -57,6 +57,8 @@ const SidebarChatList: FC<SidebarChatListProps> = ({ sessionId, friends }) => {
         return () => {
             pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`));
             pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
+            pusherClient.unbind("new_message", chatHandler);
+            pusherClient.unbind("new_friend", newFriendHandler);
         };
     }, [pathname, sessionId, router]);
 
